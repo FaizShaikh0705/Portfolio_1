649349b36274c3c220b6f5aacc40ac1676a1cbbb
FaizShaikh0705 <faizshaikh0705@gmail.com>
2023-01-24 19:25:28 +0530

    completed the portfolio project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,170 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG2 from '../../assets/portfolio1.jpg'
+import IMG1 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+
+
+// =======Generating Portfolio Items from Array using map method=====
+
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'This the title 1',
+    github: 'https://github.com/FaizShaikh0705',
+    demo: 'https://github.com/FaizShaikh0705/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'This the title 2',
+    github: 'https://github.com/FaizShaikh0705',
+    demo: 'https://github.com/FaizShaikh0705/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'This the title 3',
+    github: 'https://github.com/FaizShaikh0705',
+    demo: 'https://github.com/FaizShaikh0705/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'This the title 4',
+    github: 'https://github.com/FaizShaikh0705',
+    demo: 'https://github.com/FaizShaikh0705/'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'This the title 5',
+    github: 'https://github.com/FaizShaikh0705',
+    demo: 'https://github.com/FaizShaikh0705/'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'This the title 6',
+    github: 'https://github.com/FaizShaikh0705',
+    demo: 'https://github.com/FaizShaikh0705/'
+  }
+]
+
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+       {
+        data.map(({id, image, title, github, demo}) => {
+          return(
+            <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+            <a href={github} className="btn" target='_blank'>Github</a>
+            <a href={demo} className="btn btn-primary" target='_blank'>Live Demo</a>
+            </div>
+          </article>
+          )
+        })
+       }
+        </div>
+    </section>
   )
 }
 
 export default Portfolio
+
+
+// const Portfolio = () => {
+//   return (
+//     <section id='portfolio'>
+//       <h5>My Recent Work</h5>
+//       <h2>Portfolio</h2>
+
+//       <div className="container portfolio__container">
+//         <article className="portfolio__item">
+//           <div className="portfolio__item-image">
+//             <img src={IMG1} alt="" />
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className="portfolio__item-cta">
+//           <a href="" className="btn" target='_blank'>Github</a>
+//           <a href="" className="btn btn-primary" target='_blank'>Live Demo</a>
+//           </div>
+//         </article>
+
+//         <article className="portfolio__item">
+//           <div className="portfolio__item-image">
+//             <img src={IMG2} alt="" />
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className="portfolio__item-cta">
+//           <a href="" className="btn" target='_blank'>Github</a>
+//           <a href="" className="btn btn-primary" target='_blank'>Live Demo</a>
+//           </div>
+//         </article>
+
+//         <article className="portfolio__item">
+//           <div className="portfolio__item-image">
+//             <img src={IMG3} alt="" />
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className="portfolio__item-cta">
+//           <a href="https://github.com/FaizShaikh0705/Stop-Watch-Project" className="btn" target='_blank'>Github</a>
+//           <a href="https://faizshaikh0705.github.io/Stop-Watch-Project/" className="btn btn-primary" target='_blank'>Live Demo</a>
+//           </div>
+//         </article>
+
+//         <article className="portfolio__item">
+//           <div className="portfolio__item-image">
+//             <img src={IMG4} alt="" />
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className="portfolio__item-cta">
+//           <a href="" className="btn" target='_blank'>Github</a>
+//           <a href="" className="btn btn-primary" target='_blank'>Live Demo</a>
+//           </div>
+//         </article>
+
+//         <article className="portfolio__item">
+//           <div className="portfolio__item-image">
+//             <img src={IMG5} alt="" />
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className="portfolio__item-cta">
+//           <a href="" className="btn" target='_blank'>Github</a>
+//           <a href="" className="btn btn-primary" target='_blank'>Live Demo</a>
+//           </div>
+//         </article>
+
+//         <article className="portfolio__item">
+//           <div className="portfolio__item-image">
+//             <img src={IMG6} alt="" />
+//           </div>
+//          <h3>This is a portfolio item title</h3>
+//          <div className="portfolio__item-cta">
+//           <a href="" className="btn" target='_blank'>Github</a>
+//           <a href="" className="btn btn-primary" target='_blank'>Live Demo</a>
+//           </div>
+//         </article>
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default Portfolio
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,143 @@
 import React from 'react'
 import './Testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+
+// import required modules
+import { Pagination } from "swiper";
+
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et exercitationem, ut quisquam non reprehenderit quaerat provident? Exercitationem ratione blanditiis, beatae nemo eos porro temporibus, fugit magnam labore doloremque in cum.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Shatte Wale',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et exercitationem, ut quisquam non reprehenderit quaerat provident? Exercitationem ratione blanditiis, beatae nemo eos porro temporibus, fugit magnam labore doloremque in cum.'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Kwame Despite',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et exercitationem, ut quisquam non reprehenderit quaerat provident? Exercitationem ratione blanditiis, beatae nemo eos porro temporibus, fugit magnam labore doloremque in cum.'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Nana Ama McBrown',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et exercitationem, ut quisquam non reprehenderit quaerat provident? Exercitationem ratione blanditiis, beatae nemo eos porro temporibus, fugit magnam labore doloremque in cum.'
+  },
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonails'>Testimonials</section>
+    <section id='testimonails'>
+      <h5>Review from clients</h5>
+      <h2>Testimonails</h2>
+
+      <Swiper className="container testimonials__container" 
+          //install swiper module
+        modules={[Pagination]} spaceBetween={40}
+        slidesPerView={1}
+        pagination={{clickable: true}}>
+      {
+        data.map(({avatar, name, review}, index) => {
+          return(
+            <SwiperSlide key={index} className="testimonial">
+            <div className="client__avatar">
+              <img src={avatar} />
+            </div>
+            <h5 className="client__name">{name}</h5>
+              <small className="client__review">
+               {review}
+              </small>
+          </SwiperSlide>
+          )
+        })
+      }
+
+      </Swiper>
+    </section>
   )
 }
 
 export default Testimonials
+
+
+
+
+// const Testimonials = () => {
+//   return (
+//     <section id='testimonails'>
+//       <h5>Review from clients</h5>
+//       <h2>Testimonails</h2>
+
+//       <div className="container testimonials__container">
+//         <article className="testimonial">
+//           <div className="client__avatar">
+//             <img src={AVTR1} alt="Avatar One" />
+//           </div>
+//           <h5 className="client__name">Amaan Shaikh</h5>
+//             <small className="client__review">
+//               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+//               Et exercitationem, ut quisquam non reprehenderit quaerat provident? 
+//               Exercitationem ratione blanditiis, beatae nemo eos porro temporibus, 
+//               fugit magnam labore doloremque in cum.
+//             </small>
+//         </article>
+
+//         <article className="testimonial">
+//           <div className="client__avatar">
+//             <img src={AVTR1} alt="Avatar One" />
+//           </div>
+//           <h5 className="client__name">Amaan Shaikh</h5>
+//             <small className="client__review">
+//               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+//               Et exercitationem, ut quisquam non reprehenderit quaerat provident? 
+//               Exercitationem ratione blanditiis, beatae nemo eos porro temporibus, 
+//               fugit magnam labore doloremque in cum.
+//             </small>
+//         </article>
+
+//         <article className="testimonial">
+//           <div className="client__avatar">
+//             <img src={AVTR1} alt="Avatar One" />
+//           </div>
+//           <h5 className="client__name">Amaan Shaikh</h5>
+//             <small className="client__review">
+//               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+//               Et exercitationem, ut quisquam non reprehenderit quaerat provident? 
+//               Exercitationem ratione blanditiis, beatae nemo eos porro temporibus, 
+//               fugit magnam labore doloremque in cum.
+//             </small>
+//         </article>
+
+//         <article className="testimonial">
+//           <div className="client__avatar">
+//             <img src={AVTR1} alt="Avatar One" />
+//           </div>
+//           <h5 className="client__name">Amaan Shaikh</h5>
+//             <small className="client__review">
+//               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+//               Et exercitationem, ut quisquam non reprehenderit quaerat provident? 
+//               Exercitationem ratione blanditiis, beatae nemo eos porro temporibus, 
+//               fugit magnam labore doloremque in cum.
+//             </small>
+//         </article>
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default Testimonials
